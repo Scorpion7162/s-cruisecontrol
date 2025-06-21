9418c10b9a0b434da2959f263af125689ac6ef75
Scorpion <144194359+Scorpion7162@users.noreply.github.com>
2025-06-21 22:19:34 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,10 @@
 fx_version 'cerulean'
 author 'Scorpion'
 description 'Cruise Control script, uses ox_lib notifications.'
-version '1.0.0'
+version '1.0.2'
 game 'gta5'
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
 
 
 client_scripts {
